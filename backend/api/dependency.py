1abf769b5d9ad2bcaa21c0c0024b34d79001beb6
Godwin-T <godwinolagunju0@gmail.com>
2025-05-22 04:03:04 +0000

    change structure

--- backend/api/dependency.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Header, HTTPException
-
-API_KEY = "super-secret-key"
-
-
-def verify_admin(x_api_key: str = Header(...)):
-    if x_api_key != API_KEY:
-        raise HTTPException(status_code=401, detail="Unauthorized")
